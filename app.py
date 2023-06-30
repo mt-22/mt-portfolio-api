6ee8281a7d9f0ced3ea1a43c95ca8939a2da4518
mt-22 <marshalll.taylorrr@gmail.com>
2023-06-29 18:02:50 -0700

    origin restriction

--- app.py
+++ app.py
@@ -8,35 +8,40 @@ from email_validator import validate_email, EmailNotValidError
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 mail = Mail(app)
-CORS(app, supports_credentials=True, resources={r"*": {"origins": "https://marshalltaylor.org/*"}})
+CORS(app, supports_credentials=True, resources={r"*": {"origins": "marshalltaylor.org"}})
+# CORS(app, supports_credentials=True, resources={r"*": {"origins": "*"}})
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({
-        "foo": "bar"
-    })
 
 @app.route('/contact', methods=['POST'])
 def contact():
-    data = request.json
-    name = data['name']
-    email = data['email']
-    body = data['body']
-    try :
-        emailInfo = validate_email(email, check_deliverability=False)
-        email = emailInfo.normalized
-        msg = Message(f"Message from {name}", sender='mdtaylor.portfolio@gmail.com', recipients = ['mdtaylor.portfolio@gmail.com'])
-        msg.body = (
-        f"""Name: {name}
-Email: {email}
+    print(request.environ)
+    # if 'HTTP_ORIGIN' in request.environ and request.environ['HTTP_ORIGIN'] == "http://localhost:3000":
+    if 'HTTP_ORIGIN' in request.environ and request.environ['HTTP_ORIGIN'] == "https://marshalltaylor.org":
+        data = request.json
+        name = data['name']
+        email = data['email']
+        body = data['body']
+        try :
+            emailInfo = validate_email(email, check_deliverability=False)
+            email = emailInfo.normalized
+            msg = Message(f"Message from {name}", sender='mdtaylor.portfolio@gmail.com', recipients = ['mdtaylor.portfolio@gmail.com'])
+            msg.body = (
+            f"""Name: {name}
+    Email: {email}
 
-{body}""")
-        mail.send(msg)
-        return jsonify({
-        "status": "OK"
-    })
-    except EmailNotValidError as e:
-        return jsonify({
-            "status": str(e)
-        }), 401
+    {body}""")
+            mail.send(msg)
+            return jsonify({
+            "status": "OK"
+        })
+        except EmailNotValidError as e:
+            return jsonify({
+                "status": str(e)
+            }), 401
+    else:
+        print('invalid origin')
+        return "invalid origin"
 
+### DEBUG
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
